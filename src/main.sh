cc0ae7a045dfad4dcdb2b9d668474383d42bca05
johnlk <john@useintegral.com>
2023-05-10 16:15:12 -0400

    Add model params for temp and model name
    Add a small modifier
    
    Test updating the param value type
    
    Create test logs
    
    More testing
    
    Last test
    
    Drop the temp. param for now

--- src/main.sh
+++ src/main.sh
@@ -7,7 +7,7 @@ source "$HOME_DIR/src/gpt.sh"
 ##? Auto-reviews a Pull Request
 ##?
 ##? Usage:
-##?   main.sh --github_token=<token> --open_ai_api_key=<token> --github_api_url=<url>
+##?   main.sh --github_token=<token> --open_ai_api_key=<token> --gpt_model_name=<name> --gpt_model_temp=<temp> --github_api_url=<url>
 main() {
   eval "$(/root/bin/docpars -h "$(grep "^##?" "$HOME_DIR/src/main.sh" | cut -c 5-)" : "$@")"
 
@@ -16,10 +16,12 @@ main() {
   utils::env_variable_exist "github_token"
   utils::env_variable_exist "github_api_url"
   utils::env_variable_exist "open_ai_api_key"
+  utils::env_variable_exist "gpt_model_name"
 
   export GITHUB_TOKEN="$github_token"
   export GITHUB_API_URL="$github_api_url"
   export OPEN_AI_API_KEY="$open_ai_api_key"
+  export GPT_MODEL="$gpt_model_name"
 
   local -r pr_number=$(utils::get_pr_number)
   local -r commit_diff=$(github::get_commit_diff "$pr_number")
