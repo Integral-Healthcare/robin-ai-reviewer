d103b82e0856d2591ac133b55ca5ec5939577c81
johnlk <john@useintegral.com>
2023-05-11 17:41:11 -0400

    Drop concept of a letter grade

--- src/gpt.sh
+++ src/gpt.sh
@@ -8,10 +8,9 @@ The code will come as a git diff. If a file is deleted, do not give feedback on 
 Readme file, package.json, or any other file that seems less like code and more like libraries or configuration, you may \
 ignore it. If all files in the git diff are configs or other non-code files, you may respond with “nothing to grade” and \
 ignore the remaining instructions.\n\
-You will first give feedback in the form of a letter grade. Either A,B,C,D, or F. The letter grade will describe \
-maintainability. You will assign grades according to the following rules. An A is given for code that is so good the only \
-improvement you can describe is better comments. B is given when there is 1 improvement. C is for code with 2 improvements. \
-D is for code that has 3 improvements to be made. For code that has 4 or more improvement opportunities, assign it an F.\n\
+You will first give feedback in the form of a score between 0-100. The number will estimate how likely the code change will \
+be accepted. Assume the CTO is really stingy and only accepts high level production code, so they reject most initial code \
+changes. Do not give a justification for your 0-100 score.\n\
 Second, you will respond with a short list of improvements. Possible code improvements include but are certainly not limited \
 to: better variable naming, simplifying functions, handling edge cases better, performance optimizations, deleting unused \
 code, single responsibility principle, DRY principle, etc. You are not to give feedback on commenting with heredocs. Our \
