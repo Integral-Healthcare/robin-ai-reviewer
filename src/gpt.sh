6c7e38a800a47a2793de148ecd3f68de01c958da
johnlk <john@useintegral.com>
2023-06-25 10:09:22 -0400

    Parameterize the model prompt
    Attempt to log new vars
    
    Test
    
    Test commit
    
    Potential syntax fix

--- src/gpt.sh
+++ src/gpt.sh
@@ -1,32 +1,31 @@
 #!/usr/bin/env bash
 
-INITIAL_PROMPT=$(cat <<EOF
-I'm going to assign you a role. Your role is being a pull request code reviewer on our engineering team. As such we need you \
-to respond with some constructive feedback on our code. Your main contribution to the team is providing crisp constructive \
-feedback on how we can improve our code's quality, maintainability, and readability to name a few.\n\
-The code will come as a git diff. If a file is deleted, do not give feedback on it.\n\
-You will first give feedback in the form of a score between 0-100. The number will estimate how likely the code change will \
-be accepted. Assume the CTO is really stingy and only accepts high level production code, so they reject most initial code \
-changes. Do not give a justification for your 0-100 score.\n\
-Second, you will respond with a short list of improvements. Possible code improvements include but are certainly not limited \
-to: better variable naming, simplifying functions, handling edge cases better, performance optimizations, deleting unused \
-code, single responsibility principle, DRY principle, etc. You are not to give feedback on commenting with heredocs. Our \
-team's preference is to have self-documenting code, so we don't care about comments unless in special circumstances.\n\
-Finally, your last piece of feedback should include a code block. Important: If you assigned an score of >= 90, you should \
-not produce a code block, just repond "N/A". For scores < 90, the code block can either be a complete re-write of the code \
-being scrutinized or a subset of it, but you should illustrate your feedback with a code example. Be sure to include the \
-language tag on the code block as this response will be rendered in markdown. Do not explain the code block!\n\
-The code block should be the last part of all your responses.
-EOF
-)
-
 gpt::prompt_model() {
   local -r git_diff="$1"
+  local -r initial_prompt=$(cat <<EOF
+root_language: $2
+coding_principles: $3
+ignored_principles: $4
+Pretend you're the pull request reviewer on our software engineering team. As such we need you to respond with some constructive feedback on our code. \
+Your main contribution to the team is providing crisp constructive feedback on how we can improve our code's quality. The code is primarily written in \
+"root_language".\n\
+The code will come as a git-diff. If a file is deleted, do not give feedback on it.\n\
+You will first give feedback in the form of a score between 0-100. The number will estimate how likely the code change will be accepted. Assume the \
+team only accepts high level production code, so they reject most initial code changes. Do not give a justification for your 0-100 score.\n\
+Second, you will respond with a short list of improvements. Possible code improvements include but are certainly not limited to: "coding_principles". \
+You are not to give feedback on "ignored_principles".\n\
+Finally, you will respond with a code block. Important: If you assigned an score of >= 90, you should not produce a code block; instead respond "N/A". \
+For scores < 90, the code block can either be a complete re-write of the code being scrutinized or a subset of it, but you should illustrate your \
+feedback with a code example. Be sure to include the language tag on the code block as this response will be rendered in markdown. Do not explain the \
+code block!\n\
+The code block should be the last thing you output. Do not write any messaging after the code block.
+EOF
+)
 
   local -r body=$(curl -sSL \
     -H "Content-Type: application/json" \
     -H "Authorization: Bearer $OPEN_AI_API_KEY" \
-    -d "$(jq -n --arg model "$GPT_MODEL" --arg prompt "$INITIAL_PROMPT" --arg git_diff "$git_diff" '{model: $model, messages: [{role: "user", content: $prompt}, {role: "user", content: $git_diff}]}')" \
+    -d "$(jq -n --arg model "$GPT_MODEL" --arg prompt "$initial_prompt" --arg git_diff "$git_diff" '{model: $model, messages: [{role: "user", content: $prompt}, {role: "user", content: $git_diff}]}')" \
     "https://api.openai.com/v1/chat/completions")
 
   local -r error=$(echo "$body" | jq -r '.error')
